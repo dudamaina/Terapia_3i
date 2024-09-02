6c7d9cd998c08df89d6ccebc89903745f31d37a2
Maria Eduarda <108494852+dudamaina@users.noreply.github.com>
2024-09-02 08:51:09 -0300

    Add files via upload
    02/09

---
+++ Terapia_3i-main/Terapia_3i-main/js/entrevista.js
@@ -0,0 +1,152 @@
+let entrevista1 = {
+    entrevistador: "Fulano 1",
+    entrevistado: "fulano 2",
+    questoes: [
+        {
+            p1:"Qual curso superior você quer fazer e em qual instituição?",
+            r1:""
+        },
+        {
+            p2:"Que profissão você quer seguir?",
+            r2:""
+        },
+        {
+            p3:"Como você imagina sua vida pessoal no futuro?",
+            r3:""
+        },
+        {
+            p4:"Caso não faça nenhum curso superior, nos próximos anos, o que fará?",
+            r4:""
+        },
+        {
+            p5:"Quais são os seus maiores sonhos?",
+            r5:""
+        },
+        {
+            p6:"Qual viagem você gostaria de fazer no futuro?",
+            r6:"sarandi"
+        },
+        {
+            p7:"Qual parceiro(a) você busca?",
+            r7:""
+        },
+        {
+            p8:"Qual é o seu maior medo em relação ao futuro?",
+            r8:""
+        },
+    ]
+    }
+    let entrevista2 = {
+        entrevistador: "Fulano 2",
+        entrevistado: "fulano 3",
+        questoes: [
+            {
+                p1:"Qual curso superior você quer fazer e em qual instituição?",
+                r1:""
+            },
+            {
+                p2:"Que profissão você quer seguir?",
+                r2:""
+            },
+            {
+                p3:"Como você imagina sua vida pessoal no futuro?",
+                r3:""
+            },
+            {
+                p4:"Caso não faça nenhum curso superior, nos próximos anos, o que fará?",
+                r4:""
+            },
+            {
+                p5:"Quais são os seus maiores sonhos?",
+                r5:""
+            },
+            {
+                p6:"Qual viagem você gostaria de fazer no futuro?",
+                r6:""
+            },
+            {
+                p7:"Qual parceiro(a) você busca?",
+                r7:""
+            },
+            {
+                p8:"Qual é o seu maior medo em relação ao futuro?",
+                r8:""
+            },
+        ]
+        }
+        let entrevista3 = {
+            entrevistador: "Fulano 3",
+            entrevistado: "fulano 4",
+            questoes: [
+                {
+                    p1:"Qual curso superior você quer fazer e em qual instituição?",
+                    r1:""
+                },
+                {
+                    p2:"Que profissão você quer seguir?",
+                    r2:""
+                },
+                {
+                    p3:"Como você imagina sua vida pessoal no futuro?",
+                    r3:""
+                },
+                {
+                    p4:"Caso não faça nenhum curso superior, nos próximos anos, o que fará?",
+                    r4:""
+                },
+                {
+                    p5:"Quais são os seus maiores sonhos?",
+                    r5:""
+                },
+                {
+                    p6:"Qual viagem você gostaria de fazer no futuro?",
+                    r6:""
+                },
+                {
+                    p7:"Qual parceiro(a) você busca?",
+                    r7:""
+                },
+                {
+                    p8:"Qual é o seu maior medo em relação ao futuro?",
+                    r8:""
+                },
+            ]
+            }
+            let entrevista4 = {
+                entrevistador: "Fulano 4",
+                entrevistado: "fulano 1",
+                questoes: [
+                    {
+                        p1:"Qual curso superior você quer fazer e em qual instituição?",
+                        r1:""
+                    },
+                    {
+                        p2:"Que profissão você quer seguir?",
+                        r2:""
+                    },
+                    {
+                        p3:"Como você imagina sua vida pessoal no futuro?",
+                        r3:""
+                    },
+                    {
+                        p4:"Caso não faça nenhum curso superior, nos próximos anos, o que fará?",
+                        r4:""
+                    },
+                    {
+                        p5:"Quais são os seus maiores sonhos?",
+                        r5:""
+                    },
+                    {
+                        p6:"Qual viagem você gostaria de fazer no futuro?",
+                        r6:""
+                    },
+                    {
+                        p7:"Qual parceiro(a) você busca?",
+                        r7:""
+                    },
+                    {
+                        p8:"Qual é o seu maior medo em relação ao futuro?",
+                        r8:""
+                    },
+                ]
+                }
